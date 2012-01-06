1d7a3b8aa1a8bafbd3ea1fa93215e25219e6486f
jorgecasar <jorge.casar@gmail.com>
2012-01-06 08:21:13 +0100

    moved fn.serializeObject to the correct file

--- src/main/webapp/resources/js/plugins.js
+++ src/main/webapp/resources/js/plugins.js
@@ -17,4 +17,19 @@ window.log = function(){
 
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
-
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -301,20 +301,3 @@ logoPollSettings.stepFunctions[1] = {
 };
 
 window.LogoPoll = new stepsForm('body', logoPollSettings);
-
-$.fn.serializeObject = function()
-{
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
