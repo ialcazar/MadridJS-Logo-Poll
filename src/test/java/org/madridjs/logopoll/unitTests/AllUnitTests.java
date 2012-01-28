d0e222bd821e4be528c0f7c6ca39b0cb29c42ac3
ialcazar <israelalcazar@gmail.com>
2012-01-28 21:15:28 +0100

    Added Email Validator Test to AllUnitTests Suite

--- src/test/java/org/madridjs/logopoll/unitTests/AllUnitTests.java
+++ src/test/java/org/madridjs/logopoll/unitTests/AllUnitTests.java
@@ -5,10 +5,11 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.madridjs.logopoll.services.LoginServiceUnitTests;
 import org.madridjs.logopoll.services.MailServiceUnitTests;
+import org.madridjs.logopoll.validators.UserValidatorTest;
 import org.madridjs.logopoll.web.*;
 
 @RunWith(Suite.class)
-@SuiteClasses({LoginControllerUnitTests.class,LogosControllerUnitTests.class,VotesControllerUnitTests.class,MailServiceUnitTests.class,LoginServiceUnitTests.class})
+@SuiteClasses({LoginControllerUnitTests.class,LogosControllerUnitTests.class,VotesControllerUnitTests.class,MailServiceUnitTests.class,LoginServiceUnitTests.class,UserValidatorTest.class})
 public class AllUnitTests {
 
 }
